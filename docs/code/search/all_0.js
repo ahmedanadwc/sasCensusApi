ea1d815f54a8a03472fc4a8b30674b4966cf7622
ahmedanadwc <ahmed.al-attar@anadwc.com>
2023-08-11 07:59:04 -0400

    Updated header comments section

---
+++ docs/code/search/all_0.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['censusapi_5fgetalldatasets_2esas_0',['censusapi_getalldatasets.sas',['../censusapi__getalldatasets_8sas.html',1,'']]],
+  ['censusapi_5fgetdataapiqueryrspns_2esas_1',['censusapi_getdataapiqueryrspns.sas',['../censusapi__getdataapiqueryrspns_8sas.html',1,'']]],
+  ['censusapi_5fgetdsexamples_2esas_2',['censusapi_getdsexamples.sas',['../censusapi__getdsexamples_8sas.html',1,'']]],
+  ['censusapi_5fgetdsfullinfo_2esas_3',['censusapi_getdsfullinfo.sas',['../censusapi__getdsfullinfo_8sas.html',1,'']]],
+  ['censusapi_5fgetdsgeos_2esas_4',['censusapi_getdsgeos.sas',['../censusapi__getdsgeos_8sas.html',1,'']]],
+  ['censusapi_5fgetdsgroups_2esas_5',['censusapi_getdsgroups.sas',['../censusapi__getdsgroups_8sas.html',1,'']]],
+  ['censusapi_5fgetdsvars_2esas_6',['censusapi_getdsvars.sas',['../censusapi__getdsvars_8sas.html',1,'']]],
+  ['censusapi_5fgetgrpvars_2esas_7',['censusapi_getgrpvars.sas',['../censusapi__getgrpvars_8sas.html',1,'']]],
+  ['censusapi_5fgetusgeohierarchy_2esas_8',['censusapi_getusgeohierarchy.sas',['../censusapi__getusgeohierarchy_8sas.html',1,'']]],
+  ['censusapi_5fsubmitdataapiquery_2esas_9',['censusapi_submitdataapiquery.sas',['../censusapi__submitdataapiquery_8sas.html',1,'']]]
+];
